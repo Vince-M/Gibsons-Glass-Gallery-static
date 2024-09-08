c65279d14769393101493c161583434231f78690
Vince Mendella <vmendella@gmail.com>
2024-09-08 16:17:38 -0500

    add hiding elements for accessibility

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,29 @@
 console.log('Hello');
+
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const breakpoint = window.matchMedia('(width < 43.75em)');
+
+if (breakpoint.matches) {
+  console.log('is mobile');
+  menuTopNav.setAttribute('inert', '');
+} else {
+  console.log('is tablet/desktop');
+  menuTopNav.removeAttribute('inert');
+}
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+function openMobileMenu() {
+  console.log('run openMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'true');
+  menuTopNav.removeAttribute('inert');
+}
+
+function closeMobileMenu() {
+  console.log('run closeMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'false');
+  menuTopNav.setAttribute('inert', '');
+}
